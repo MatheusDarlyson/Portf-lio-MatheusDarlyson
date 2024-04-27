4d51a4f3f3a07bf45c01d1de60184a4e0c9f34d3
MatheusDarlyson <matheus.darlyson19@gmail.com~>
2024-04-26 21:20:50 -0300

    atualização portfolio

--- scripts.js
+++ scripts.js
@@ -7,3 +7,17 @@ window.addEventListener('scroll', () => {
         header.style.background = 'Transparent'
     }
 })
+
+
+    let swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      }
+    });
